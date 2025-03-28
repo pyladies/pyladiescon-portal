178d592cf5d4324b71d6e3017cc15572d1317172
Pradhvan <Pradhvanbisht@gmail.com>
2025-03-28 23:02:20 +0530

    Allow user to add multiple emails (#48)
    - User can have max 3 emails.
    - Updated Change Email address button -> Manage Emails for more generic
      email actions.
    - Updated HTML templates to reflect Change Email address -> Manage Emails.
    
    Closes https://github.com/pyladies/pyladiescon-portal/issues/44
    ---------
    
    Co-authored-by: Mariatta <Mariatta@users.noreply.github.com>

--- portal/settings.py
+++ portal/settings.py
@@ -164,7 +164,7 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = "[PyLadiesCon Dev] "
 LOGIN_REDIRECT_URL = "/"
 ACCOUNT_LOGIN_METHODS = {"username"}
 ACCOUNT_SIGNUP_FIELDS = ["email*", "username*", "first_name*", "last_name*", "password1*", "password2*"]
-ACCOUNT_CHANGE_EMAIL = True
+ACCOUNT_MAX_EMAIL_ADDRESSES = 3
 
 # Use custom signup form
 ACCOUNT_FORMS = {
