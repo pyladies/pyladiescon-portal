970bfaa3a1d2d34d70a989fe488757b74d792bfe
Farhan Ali Raza <62690310+FarhanAliRaza@users.noreply.github.com>
2025-03-28 22:23:34 +0500

    Added fname, lname at signup (#46)

--- portal/settings.py
+++ portal/settings.py
@@ -163,9 +163,13 @@ ACCOUNT_EMAIL_VERIFICATION_BY_CODE_ENABLED = True
 ACCOUNT_EMAIL_SUBJECT_PREFIX = "[PyLadiesCon Dev] "
 LOGIN_REDIRECT_URL = "/"
 ACCOUNT_LOGIN_METHODS = {"username"}
-ACCOUNT_SIGNUP_FIELDS = ["email*", "username*", "password1*", "password2*"]
+ACCOUNT_SIGNUP_FIELDS = ["email*", "username*", "first_name*", "last_name*", "password1*", "password2*"]
 ACCOUNT_CHANGE_EMAIL = True
 
+# Use custom signup form
+ACCOUNT_FORMS = {
+    'signup': 'portal.forms.CustomSignupForm'
+}
 
 # Default settings
 BOOTSTRAP5 = {
