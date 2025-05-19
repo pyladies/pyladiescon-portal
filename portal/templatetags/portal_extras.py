6c8349ef9a58ffffdedac0f0025a268b3c4541f7
M <lmiguelvargasf@gmail.com>
2025-05-19 17:33:09 -0400

    Add docstring to get_item template filter for clarity and usage guidance

--- portal/templatetags/portal_extras.py
+++ portal/templatetags/portal_extras.py
@@ -5,4 +5,8 @@ register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
+    """
+    Template filter to get a value from a dictionary by key,
+    returning None if not found.
+    """
     return dictionary.get(key)
