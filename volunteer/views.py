cb94e66fab953302b4d9b86fd479a68954feaef4
Carolina Gómez Trejos <carolina9511@gmail.com>
2025-05-19 15:46:25 -0400

    fix linting

--- volunteer/views.py
+++ volunteer/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import VolunteerProfileForm
 from .models import Team, VolunteerProfile
