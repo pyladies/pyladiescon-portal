7ae5d034d7a97f5f7506e3af233ff2998f741790
mariatta <mariatta.wijaya@gmail.com>
2025-06-09 15:38:31 -0700

    Remove unused import

--- portal/validators.py
+++ portal/validators.py
@@ -1,13 +1,14 @@
 import re
 
-from django.core.exceptions import ValidationError
-
 
 def validate_linked_in_pattern(value):
     """Validate that the passed in value matches the LinkedIn URL pattern.
 
     Support personal, school, and company urls.
     For now, just check if it starts with the linkedin url.
+
+    Returns True if it starts with the LinkedIn URL.
+    Returns False otherwise.
     """
 
     linkedin_pattern = r"^(https?://)?(www\.)?linkedin\.com/(in|company|school)/"
