c7600580b17a78235f35f3d7bb06b95c8b01baab
Mariatta <Mariatta@users.noreply.github.com>
2025-06-10 15:31:13 -0700

    Revert changes to see if it would fix deployment (#184)

--- tests/portal/test_common.py
+++ tests/portal/test_common.py
@@ -1,10 +1,11 @@
 import pytest
 from django.contrib.auth import get_user_model
-from django.core.cache import cache
 
 from portal.common import get_stats_cached_values, get_volunteer_signup_stat_cache
 from portal.constants import CACHE_KEY_VOLUNTEER_SIGNUPS_COUNT
 from volunteer.models import VolunteerProfile
+
+# from django.core.cache import cache
 
 
 @pytest.mark.django_db
@@ -13,8 +14,8 @@ class TestGetStatsCachedValues:
     def test_get_volunteer_signup_stat_cache(self):
         """Test that the volunteer signup count is cached and returned correctly."""
 
-        cache_key = "volunteer_signups_count"
-        cache.delete(cache_key)
+        # cache_key = CACHE_KEY_VOLUNTEER_SIGNUPS_COUNT
+        # cache.delete(cache_key)
 
         result = get_volunteer_signup_stat_cache()
         assert result == 0
@@ -34,8 +35,8 @@ class TestGetStatsCachedValues:
     def test_get_stats_cached_values(self):
         """Test that the stats dictionary contains the volunteer signup count."""
 
-        cache_key = CACHE_KEY_VOLUNTEER_SIGNUPS_COUNT
-        cache.delete(cache_key)
+        # cache_key = CACHE_KEY_VOLUNTEER_SIGNUPS_COUNT
+        # cache.delete(cache_key)
 
         stats = get_stats_cached_values()
         assert stats[CACHE_KEY_VOLUNTEER_SIGNUPS_COUNT] == 0
