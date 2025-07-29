a1e7c8450a8f13e28625a12b7a72b49ebd655d2f
Lucille Idokogi <lidokogi@uwo.ca>
2025-07-28 23:11:12 -0400

    Implement sponsorship email logic and SponsorshipTier model

--- sponsorship/admin.py
+++ sponsorship/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import SponsorshipProfile
 
-admin.site.register(SponsorshipProfile)
+@admin.register(SponsorshipProfile)
+class SponsorshipProfileAdmin(admin.ModelAdmin):
+    list_display = ('organization_name', 'main_contact_user','sponsorship_type', 'application_status')
+    list_filter = ('sponsorship_type', 'application_status', 'sponsorship_tier')
+    search_fields = ('sponsor_organization_name', 'main_contact__username')
